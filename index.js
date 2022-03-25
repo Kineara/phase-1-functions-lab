7d385b1f52f5bd8408d317afce3bd155ea4a387a
Lauren Hoock <laurenh8916@protonmail.com>
2022-03-25 01:27:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const hqLocation = 42;
+const blockLengthInFt = 264;
+
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(hqLocation - pickupLocation);
+};
+
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * blockLengthInFt;
+};
+
+function distanceTravelledInFeet(pickupBlock, dropoffBlock){
+    return (Math.abs(pickupBlock - dropoffBlock) * blockLengthInFt);
+};
+
+function calculatesFarePrice(pickupBlock, dropoffBlock){
+    let distInFeet = distanceTravelledInFeet(pickupBlock, dropoffBlock);
+    if (distInFeet > 2500){
+        return 'cannot travel that far';
+    } else if (distInFeet > 2000){
+        return 25;
+    } else if (distInFeet > 400) {
+        return (distInFeet - 400) * .02;
+    } else {
+        return 0;
+    }
+    
+};
